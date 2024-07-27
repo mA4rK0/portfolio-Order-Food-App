61d5ec5025f9a35d8bed9669f7065ff85196bf73
mA4rK0 <onlymarko25@gmail.com>
2024-07-27 21:03:46 +0700

    adding images for menu.

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -7,12 +7,12 @@ function Menu() {
     <>
       {foods.map((category, index) => (
         <section key={index} className="min-[320px]:w-[360px] mx-auto mt-8 grid grid-cols-2 gap-y-7 justify-items-center">
-          {category.items.map(({ name, price }, indexKey) => (
+          {category.items.map(({ name, price, image }, indexKey) => (
             <section key={indexKey} className="border w-40 h-56 rounded-3xl flex flex-col items-center pt-6 bg-zinc-50 cursor-pointer">
-              <img className="w-[8rem] h-[6.188rem] rounded-2xl" src="../src/img/thumb/Chicken Noodles (1).jpg" alt="Chicken Noodles" />
-              <section className="flex flex-wrap text-lg font-semibold justify-center pt-9">
-                <p className="w-[6.188rem]">{name}</p>
-                <p className="text-yes pt-[1.71rem]">${price}</p>
+              <img className="w-[8rem] h-[6.188rem] rounded-2xl" src={`../src/img/thumb/${image}`} alt="Chicken Noodles" />
+              <section className="flex flex-col text-lg font-semibold items-center pt-3">
+                <p className="text-center">{name}</p>
+                <p className="text-yes">${price}</p>
               </section>
             </section>
           ))}
