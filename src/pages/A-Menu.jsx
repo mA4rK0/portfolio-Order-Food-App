bba35c74106fa161bce306fb8fa37e9a5eec41c2
mA4rK0 <onlymarko25@gmail.com>
2024-08-07 14:31:33 +0700

    design a menu page.

--- src/pages/A-Menu.jsx
+++ src/pages/A-Menu.jsx
@@ -29,14 +29,14 @@ export default function AMenu() {
   } else {
     return (
       <>
-        <section className="container text-white">
+        <section className="container text-white w-screen">
           <section className="top-element">
             <button onClick={(e) => handleBtn(e)}>
               <i className="mt-4 ml-3 text-2xl fa-solid fa-arrow-left"></i>
             </button>
             <img src={`../src/img/thumb/${menu.image}`} className="my-6 mx-auto w-72 h-[13.296rem]" />
           </section>
-          <section className="bottom-element bg-input rounded-tl-[2.5rem]">
+          <section className="bottom-element bg-input rounded-l-[2.5rem] w-screen">
             <section className="flex flex-col pt-4 gap-y-3">
               <section className="flex flex-wrap justify-between px-5 items-center">
                 <p className="font-semibold text-2xl rounded-full bg-black p-1">{menu.status}</p>
@@ -54,7 +54,12 @@ export default function AMenu() {
                   </section>
                 </section>
               </section>
-              <p className="flex items-center pl-5">{menu.description}</p>
+              <section className="flex flex-col">
+                <p className="pl-5 items-center">{menu.description}</p>
+                <button className="mx-auto my-6 px-14 rounded-lg font-semibold text-2xl hover:opacity-80 border-yes bg-yes ease-in-out duration-300 transition">
+                  Add to cart <i className="fa-solid fa-cart-shopping"></i>
+                </button>
+              </section>
             </section>
           </section>
         </section>
